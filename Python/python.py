2618f6f34d1038807eef79e556aca695f319debf
Michael Miller <mmiller@openintel.org>
2018-06-28 00:51:33 -0400

    Some random scripts and programs from cybrary and a disclaimer readme.

---
+++ Python/python.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import socket
+ip = raw_input("Enter the IP Address: ")
+port = input("Enter the Port Number: ")
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if sock.connect_ex((ip,port)):
+	print "Port ", port, " is closed"
+else:
+	print "Port ", port, " is open"
